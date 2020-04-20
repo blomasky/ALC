ea0d4177bbe8ac10d0d19f0905c30e2e21f5f11b
bruce <bruce@HAL-10K>
2020-04-20 14:42:00 -0400

    Add project files.

---
+++ app/App_Code/Services/ApplicationServices.generated.cs
@@ -0,0 +1,28 @@
+﻿using FreeTrial.Handlers;
+using FreeTrial.Data;
+using FreeTrial.Web;
+using System.Web.Configuration;
+
+namespace FreeTrial.Services
+{
+	public class AppFrameworkConfig
+    {
+        
+        public virtual void Initialize()
+        {
+            ApplicationServices.FrameworkAppName = "American Lumber Co. Inc.";
+            ApplicationServices.Version = "8.9.4.0";
+            ApplicationServices.JqmVersion = "1.4.6";
+            ApplicationServices.HostVersion = "1.2.5.0";
+            var releaseMode = true;
+            AquariumExtenderBase.EnableMinifiedScript = releaseMode;
+            AquariumExtenderBase.EnableCombinedScript = releaseMode;
+            ApplicationServices.EnableMinifiedCss = releaseMode;
+            ApplicationServices.EnableCombinedCss = releaseMode;
+            CultureManager.SupportedCultures = new string[] {
+                    "en-US,en-US"};
+            ApplicationServicesBase.AuthorizationIsSupported = false;
+            BlobFactoryConfig.Initialize();
+        }
+    }
+}
